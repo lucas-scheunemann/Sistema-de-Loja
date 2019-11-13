927e0d4be7c610f48137733dca8d3ad6fc8c768c
Lucas Scheunemann <l182441@g.unicamp.br>
2019-11-13 15:07:47 -0300

    adição da base do sistema
    Com utilização da forma de divisão entre partes do sistema .NET em 4 camadas, o sistema ficou separado e modularizado.

---
+++ lucas/Sistema_Loja/MDIParent1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Sistema_Loja
+{
+	partial class MDIParent1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.Cadastro = new System.Windows.Forms.ToolStripMenuItem();
+			this.Estoque = new System.Windows.Forms.ToolStripMenuItem();
+			this.Sobre = new System.Windows.Forms.ToolStripMenuItem();
+			this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.entradaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.informaçõesDoSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip.SuspendLayout();
+			this.statusStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Cadastro,
+            this.Estoque,
+            this.Sobre});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(632, 24);
+			this.menuStrip.TabIndex = 0;
+			this.menuStrip.Text = "MenuStrip";
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+			this.statusStrip.Location = new System.Drawing.Point(0, 431);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(632, 22);
+			this.statusStrip.TabIndex = 2;
+			this.statusStrip.Text = "StatusStrip";
+			// 
+			// toolStripStatusLabel
+			// 
+			this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+			this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+			this.toolStripStatusLabel.Text = "Status";
+			// 
+			// Cadastro
+			// 
+			this.Cadastro.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.produtosToolStripMenuItem});
+			this.Cadastro.Name = "Cadastro";
+			this.Cadastro.Size = new System.Drawing.Size(66, 20);
+			this.Cadastro.Text = "Cadastro";
+			// 
+			// Estoque
+			// 
+			this.Estoque.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.entradaToolStripMenuItem,
+            this.saidaToolStripMenuItem});
+			this.Estoque.Name = "Estoque";
+			this.Estoque.Size = new System.Drawing.Size(61, 20);
+			this.Estoque.Text = "Estoque";
+			// 
+			// Sobre
+			// 
+			this.Sobre.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informaçõesDoSistemaToolStripMenuItem});
+			this.Sobre.Name = "Sobre";
+			this.Sobre.Size = new System.Drawing.Size(49, 20);
+			this.Sobre.Text = "Sobre";
+			// 
+			// usuariosToolStripMenuItem
+			// 
+			this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+			this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.usuariosToolStripMenuItem.Text = "usuarios";
+			this.usuariosToolStripMenuItem.Click += new System.EventHandler(this.usuariosToolStripMenuItem_Click);
+			// 
+			// produtosToolStripMenuItem
+			// 
+			this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+			this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.produtosToolStripMenuItem.Text = "Produtos";
+			this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
+			// 
+			// entradaToolStripMenuItem
+			// 
+			this.entradaToolStripMenuItem.Name = "entradaToolStripMenuItem";
+			this.entradaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.entradaToolStripMenuItem.Text = "Entrada";
+			// 
+			// saidaToolStripMenuItem
+			// 
+			this.saidaToolStripMenuItem.Name = "saidaToolStripMenuItem";
+			this.saidaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.saidaToolStripMenuItem.Text = "Saida";
+			// 
+			// informaçõesDoSistemaToolStripMenuItem
+			// 
+			this.informaçõesDoSistemaToolStripMenuItem.Name = "informaçõesDoSistemaToolStripMenuItem";
+			this.informaçõesDoSistemaToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+			this.informaçõesDoSistemaToolStripMenuItem.Text = "Informações do Sistema";
+			// 
+			// MDIParent1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(632, 453);
+			this.Controls.Add(this.statusStrip);
+			this.Controls.Add(this.menuStrip);
+			this.IsMdiContainer = true;
+			this.MainMenuStrip = this.menuStrip;
+			this.Name = "MDIParent1";
+			this.Text = "Sistema de Loja";
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		#endregion
+
+
+		private System.Windows.Forms.MenuStrip menuStrip;
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+		private System.Windows.Forms.ToolTip toolTip;
+		private System.Windows.Forms.ToolStripMenuItem Cadastro;
+		private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem Estoque;
+		private System.Windows.Forms.ToolStripMenuItem Sobre;
+		private System.Windows.Forms.ToolStripMenuItem entradaToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saidaToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem informaçõesDoSistemaToolStripMenuItem;
+	}
+}
+
+
+
---
+++ lucas/Sistema_Loja/MDIParent1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Loja
+{
+	public partial class MDIParent1 : Form
+	{
+		
+
+		public MDIParent1()
+		{
+			InitializeComponent();
+		}
+
+		private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void usuariosToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Form childForm = new CadastrarUsuario();
+			childForm.MdiParent = this;
+			childForm.Show();
+
+		}
+	}
+}
