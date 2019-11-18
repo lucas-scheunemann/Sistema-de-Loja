22c46510c9647fc9dfdb8cca9ca611e4379e7879
Lucas Scheunemann <l182441@g.unicamp.br>
2019-11-18 11:53:23 -0300

    adição do funcionamento da lista de cadastro de produto
    dentro de DAL, definição da busca por comando SELECT para recolher todas as inserções na tabela de dados de produtos.

--- lucas/Loja.DAL/ProdutoDAL.cs
+++ lucas/Loja.DAL/ProdutoDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +18,47 @@ namespace Loja.DAL
 
 		public IList<produto_DTO> cargaProduto()
 		{
+			try
+			{
+				/*conexão com BD
+				 Seleciona todos os dados da tabela produtos*/
+				SqlConnection CON = new SqlConnection();
+				CON.ConnectionString = Properties.Settings.Default.CST;
+				SqlCommand COM = new SqlCommand();
+				COM.CommandType = System.Data.CommandType.Text;
+				COM.CommandText = "SELECT * FROM tb_produtos";
+				COM.Connection = CON;
 
-			IList<produto_DTO> listProdutoDTO = new List<produto_DTO>();
+				SqlDataReader DR;
+				IList<produto_DTO> listProdutoDTO = new List<produto_DTO>();
 
-			return listProdutoDTO;
+				CON.Open();
+				DR = COM.ExecuteReader();
+				if (DR.HasRows)
+				{
+					while (DR.Read())
+					{
+						produto_DTO pdt = new produto_DTO();
+						/*nome dos objetos criados em produtos_DTO
+						 Cada objeto é privado para a lista, possibilitando que no final
+						 você tenha uma lista com todos os produtos*/
+						pdt.cod_produto = Convert.ToInt32(DR["cod_produto"]);
+						pdt.nome = Convert.ToString(DR["nome"]);
+						pdt.preco = Convert.ToDouble(DR["preco"]);
+						pdt.quantidade = Convert.ToInt32(DR["quantidade"]);
+
+						listProdutoDTO.Add(pdt);
+
+					}
+				}
+
+
+				return listProdutoDTO;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
 		}
 		public int insereProduto(produto_DTO PDT)
 		{
